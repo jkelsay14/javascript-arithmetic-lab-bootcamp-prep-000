55edeffcb39c12c0dd1d81450cb8f14879f5f77a
Jared Kelsay <jaredkelsay@gmail.com>
2018-02-03 15:16:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(a, b) {
+  return a + b
+  
+}
+function subtract(a, b) {
+    return a -= b
+}
+function multiply(a, b) {
+ return a *= b
+
+}
+function divide(a, b) {
+ return a /= b
+ 
+}
+function inc(n) {
+ return n += 1
+}
+function dec(n) {
+return  n -= 1
+
+}
+function makeInt(n) {
+  if (a=parseInt(a)) {
+  console.log(parseInt(a, 10))
+  }
+  else console.log("NaN")
+  var a="2.222"
+  console.log(parseInt(a, 10))
+  a=0*2328
+  console.log(parseInt(a, 10))
+  a="sldkjflksjf"
+  return parseInt(a, 10)
+}
+function preserveDecimal(n) {
+  if (a=parseFloat(a)) {
+    console.log(parseFloat(a))
+  }
+  else {
+    console.log("NaN")
+  }
+  var a="2.222"
+  return parseFloat(a)
+  a="sldkjflksjf"
+  return parseFloat(a)
+  }
+
